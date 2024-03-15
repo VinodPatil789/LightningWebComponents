f3d8757d9b805f0581e4d3801654214d3589f51e
VinodPatil789 <108537540+VinodPatil789@users.noreply.github.com>
2024-03-15 15:29:55 +0530

    Update createScratchOrg.groovy

--- createScratchOrg.groovy
+++ createScratchOrg.groovy
@@ -12,6 +12,15 @@ node {
 		def JWT_KEY_CRED_ID ="sfserverkey"
 		def CONNECTED_APP_CONSUMER_KEY="${params.CONNECTED_APP_CONSUMER_KEY}"
 
+		def command(script) {
+		    if (isUnix()) {
+		        return sh(returnStatus: true, script: script);
+		    } else {
+		        return bat(returnStatus: true, script: script);
+		    }
+		}
+		
+
 
 
 		// -------------------------------------------------------------------------
