97f1be4ac371d9ed56a4a472958bb568a48654e8
VinodPatil789 <108537540+VinodPatil789@users.noreply.github.com>
2024-03-15 15:24:23 +0530

    Create ScratchOrgGroovy.groovy

---
+++ ScratchOrgGroovy.groovy
@@ -0,0 +1,88 @@
+#!groovy
+
+import groovy.json.JsonSlurperClassic
+
+node {
+	try{
+		def SF_USERNAME
+		def BUILD_NUMBER="${env.BUILD_NUMBER}"
+		def RUN_ARTIFACT_DIR 
+		def HUB_ORG_USERNAME = "${params.HUB_ORG_USERNAME}"
+		def SFDC_HOST = "${params.SFDC_HOST}"
+		def JWT_KEY_CRED_ID ="sfserverkey"
+		def CONNECTED_APP_CONSUMER_KEY="${params.CONNECTED_APP_CONSUMER_KEY}"
+    def toolbelt = tool 'toolbelt'
+
+
+
+		// -------------------------------------------------------------------------
+		// Check out code from source control.
+		// -------------------------------------------------------------------------
+
+		stage('checkout source') {
+			checkout scm
+		}
+
+
+		// -------------------------------------------------------------------------
+		// Run all the enclosed stages with access to the Salesforce
+		// JWT key credentials.
+		// -------------------------------------------------------------------------
+		
+			
+			withCredentials([file(credentialsId: JWT_KEY_CRED_ID, variable: 'server_key_file')]) {
+		
+			// -------------------------------------------------------------------------
+				// Authorize the Dev Hub org with JWT key and create scratch org
+				// -------------------------------------------------------------------------
+
+				stage('Authorize DevHub And Create Scratch Org') {
+					rc = command "${toolbelt}/sf org login jwt --instance-url ${SFDC_HOST} --client-id ${CONNECTED_APP_CONSUMER_KEY} --username ${HUB_ORG_USERNAME} --jwt-key-file ${server_key_file} --setdefaultdevhubusername"
+					if (rc != 0) {
+						error 'Dev hub org authorization failed.'
+					}
+				
+
+					rmsg = command "${toolbelt}/sfdx force:org:create --definitionfile config/project-scratch-def.json --json --setdefaultusername -d 30"
+					printf rmsg
+					
+					def jsonSlurper = new JsonSlurperClassic();
+					def robj = jsonSlurper.parseText(rmsg)
+					if (robj != 0) {
+						error 'Salesforce scratch org creation failed.' + robj.message
+					}
+					SF_USERNAME = robj.result.uesrname
+					robj = null
+					
+				}
+				
+				// -------------------------------------------------------------------------
+				// Display scratch org info.
+				// -------------------------------------------------------------------------
+
+				/*stage('Display Scratch Org Info') {
+					password = sh returnStdout : true, script :"sfdx force:user:password:generate --targetusername ${SF_USERNAME}"
+					passwordOutput = sh returnStdout : true, script : "sf org display --targetusername ${SF_USERNAME}"
+					
+					echo passwordOutput
+
+				}*/
+		
+		}
+
+    def command(script) {
+    if (isUnix()) {
+        return sh(returnStatus: true, script: script);
+    } else {
+		return bat(returnStatus: true, script: script);
+    }
+}
+	}
+	
+	finally{
+		emailext (body : "Scratch of was created successfully ${currentBuild.currentResult}: job ${env.JOB_NAME}",
+				subject : "Scratch of was created successfully",
+				to : "vinod.patil789@gmail.com")
+		
+	}
+}
